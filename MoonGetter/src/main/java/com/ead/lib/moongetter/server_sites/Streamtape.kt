e2de3a40dd8a4b9a0043bfb5a949b07deb791f86
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-19 18:43:00 -0500

    new server Streamtape

--- MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Streamtape.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Streamtape.kt
@@ -1,25 +1,22 @@
 package com.ead.lib.moongetter.server_sites
 
 import android.content.Context
+import com.ead.lib.moongetter.R
+import com.ead.lib.moongetter.core.Properties
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
-import com.ead.lib.moongetter.utils.PatternManager
 
 class Streamtape(context: Context, url : String) : Server(context,url) {
 
+    private val interceptionRegex = """https://streamtape\.com/get_video\?id=[\w-]+&expires=\d+&ip=[\w-]+&token=[\w-]+&stream=\d+""".toRegex()
+    private val endingRegex = """https://streamtape\.com/favicon\.ico""".toRegex()
+
     override suspend fun onExtract() {
-        initializeBrowser()
+        initializeBrowser(false)
 
-        val sourceCode = getSourceCodeFrom(url) ?: "null"
+        url = getInterceptionUrl(url,interceptionRegex,endingRegex) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down,Properties.StreamtapeIdentifier))
+        releaseBrowser()
 
-        val getVideoMetadata  = PatternManager.singleMatch(
-            string = sourceCode,
-            regex = """document\.getElementById\('robotlink'\)\.innerHTML\s*=\s*'.*?(get_video\?[^']+)'""",
-            groupIndex = 1
-        ) ?:   throw InvalidServerException("Streamtape resource couldn't find it or resource down")
-
-        this@Streamtape.url = "https://streamtape.com/$getVideoMetadata"
         addDefault()
     }
-
 }
