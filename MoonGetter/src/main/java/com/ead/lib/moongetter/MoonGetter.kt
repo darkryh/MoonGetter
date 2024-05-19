6b13368c65dac26140aa7df24f705e3f9374071e
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-19 18:57:03 -0500

    Commented Static Class

--- MoonGetter/src/main/java/com/ead/lib/moongetter/MoonGetter.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/MoonGetter.kt
@@ -4,10 +4,25 @@ import android.content.Context
 import com.ead.lib.moongetter.models.Initializer
 import com.ead.lib.moongetter.models.Request
 
+/**
+ * Main class for MoonGetter.
+ */
 open class MoonGetter {
 
     companion object {
 
+
+        /**
+         * Initializes a new Request.Builder object with the provided context.
+         *
+         * Params:
+         *
+         * context - The context of the application.
+         *
+         * Returns:
+         *
+         * The Request.Builder object.
+         */
         fun initialize(context: Context) : Request.Builder {
             return Initializer.Builder()
                 .initialize(context)
