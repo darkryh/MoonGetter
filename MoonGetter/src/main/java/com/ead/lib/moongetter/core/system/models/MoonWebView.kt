bda6c81ed3c36e49c3db36413c287b229bc56b1e
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-19 18:49:58 -0500

    updated MoonWebView

--- MoonGetter/src/main/java/com/ead/lib/moongetter/core/system/models/MoonWebView.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/core/system/models/MoonWebView.kt
@@ -18,8 +18,6 @@ class MoonWebView @JvmOverloads constructor(
     init {
         settings.apply {
             javaScriptEnabled = true
-            domStorageEnabled = true
-            userAgentString = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
         }
 
         setDownloadListener { url, _, _, _, _ ->
