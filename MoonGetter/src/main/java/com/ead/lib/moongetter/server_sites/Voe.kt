1f9aa1bc1c4c02e74c35a8d14fde6e2afd73d90b
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-19 18:40:34 -0500

    New server Voe

--- MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Voe.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Voe.kt
@@ -1,6 +1,29 @@
 package com.ead.lib.moongetter.server_sites
 
 import android.content.Context
+import com.ead.lib.moongetter.R
+import com.ead.lib.moongetter.core.Properties
+import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.utils.PatternManager
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class Voe(context: Context, url : String) : Server(context,url)
+class Voe(context: Context, url : String) : Server(context,url) {
+
+    override suspend fun onExtract() {
+        val response = OkHttpClient()
+            .newCall(Request.Builder().url(url).build())
+            .await()
+
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down,Properties.VoeIdentifier))
+
+        url = PatternManager.singleMatch(
+            string = response.body?.string().toString(),
+            regex = """(https://\S+\.m3u8\?.*?(?=&node=)[^"]+)"""
+        ) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down,Properties.VoeIdentifier))
+
+        addDefault()
+    }
+}
