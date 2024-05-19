26bcf0de891d5e6fd27b5f3be112234eba68b036
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-19 18:50:08 -0500

    new Properties

--- MoonGetter/src/main/java/com/ead/lib/moongetter/core/Properties.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/core/Properties.kt
@@ -7,9 +7,31 @@ object Properties {
 
     const val okruUserAgent = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
 
+    const val AnonfilesIdentifier = "Anonfiles"
+    const val BayfilesIdentifier  = "Bayfiles"
+    const val FembedIdentifier = "Fembed"
+    const val FilelionsIdentifier = "Filelions"
+    const val FilemoonIdentifier = "Filemoon"
+    const val FireloadIdentifier = "Fireload"
+    const val GofileIdentifier = "Gofile"
+    const val GoogleDriveIdentifier = "GoogleDrive"
+    const val MediafireIdentifier = "Mediafire"
+    const val OkruIdentifier = "Okru"
+    const val OneFichierIdentifier = "OneFichier"
+    const val PixelDrainIdentifier = "PixelDrain"
+    const val SendvidIdentifier = "Sendvid"
+    const val StreamSbIdentifier = "StreamSb"
+    const val StreamtapeIdentifier = "Streamtape"
+    const val StreamWishIdentifier = "StreamWish"
+    const val VoeIdentifier = "Voe"
+    const val VidguardIdentifier = "Vidguard"
+
+
     const val Anonfiles  = "https?:\\/\\/(www\\.)?(anonfile)\\.com\\/\\?.+"
     const val Bayfiles  = "https?:\\/\\/(www\\.)?(bayfiles)\\.com\\/\\?.+"
     const val Fembed = "https?:\\/\\/(www\\.)?(fembed|vcdn)\\.[^\\/,^\\.]{2,}\\/(v|f)\\/.+"
+    const val Filelions = """https?://(?:www\.)?(filelions\.site)/v/[a-zA-Z0-9]+"""
+    const val Filemoon = "https?://filemoon\\.sx/[^/]+"
     const val Fireload = "https?:\\/\\/(www\\.)?(fireload)\\.com\\/.+"
     const val Gofile = """https://gofile\.io/d/(\w+)"""
     const val GoogleDrive = "https?:\\/\\/(www\\.)?drive\\.google\\.com\\/(?:file\\/d\\/|open\\?id=|uc\\?id=)([\\w-]+)"
@@ -20,8 +42,8 @@ object Properties {
     const val Sendvid = "https?:\\/\\/sendvid\\.com\\/\\w+"
     const val StreamSb = "https?://(?:sblanh|lvturbo|sbface|sbbrisk|sbchill|sblongvu|sbanh|playersb|embedsb|sbspeed|tubesb|sbrity)\\.com/(?:e|d)/\\w+\\.html"
     const val Streamtape = "https?://(?:streamtape|gettapeads)\\.com/e/\\w+"
-    const val Voe = "https?://(?:markstyleall|voe)\\.com/(?:e|d)/\\w+"
-    const val StreamWish = "https?://(?:embedwish|streamwish|cdnwish|jodwish)\\.(?:com|to)/e/\\w+"
-
+    const val Voe = "https?://(?:voe|markstyleall|shannonpersonalcost|cindyeyefinal)\\.com/(?:e|d)/\\w+"
+    const val StreamWish = "https?://(?:embedwish|streamwish|cdnwish|jodwish|swhoi)\\.(?:com|to)/e/\\w+"
+    const val Vidguard = "https:\\/\\/listeamed\\.net\\/e\\/[a-zA-Z0-9]+"
 
 }
