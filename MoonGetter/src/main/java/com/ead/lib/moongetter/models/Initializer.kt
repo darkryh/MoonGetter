808b00e8a44539eea6c9074eec4af660ff0981c2
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-21 10:44:00 -0500

    added with Builder pattern and documented

--- MoonGetter/src/main/java/com/ead/lib/moongetter/models/Initializer.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/models/Initializer.kt
@@ -6,13 +6,44 @@ open class Initializer(
     builder : Builder
 ) {
 
+    /**
+     * The context of the application.
+     */
     @get:JvmName("context") val context: Context? = builder.context
+
+
+    /**
+     * The request configuration of the connection.
+     */
     @get:JvmName("request") val request: Request.Builder = builder.request
 
-    class Builder constructor() {
+    class Builder() {
+
+
+        /**
+         * Internal variable to store the context of the application.
+         */
         internal var context : Context? = null
+
+
+        /**
+         * Internal variable to store the request configuration of the connection.
+         */
         internal var request : Request.Builder = Request.Builder()
 
+
+
+        /**
+         * Initializes a new Request.Builder object with the provided context.
+         *
+         * Params:
+         *
+         * context - The context of the application.
+         *
+         * Returns:
+         *
+         * The Request.Builder object.
+         */
         fun initialize(context: Context) : Request.Builder {
             this.context = context
 
