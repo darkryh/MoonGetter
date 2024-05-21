d3e2ddd5b0bbd448ed17f1cb9f587a58a3a3352d
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-21 10:55:21 -0500

    Example of adding your custom Server Class in the MoonGetterLib

---
+++ app/src/main/java/com/ead/project/moongetter/domain/custom_servers/SenvidModified.kt
@@ -0,0 +1,40 @@
+package com.ead.project.moongetter.domain.custom_servers
+
+import android.content.Context
+import com.ead.lib.moongetter.R
+import com.ead.lib.moongetter.core.Properties
+import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.utils.PatternManager
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class SenvidModified(context : Context,url :String) : Server(context,url) {
+
+    private val urlRegex =  """https://custom\.domain\.com/aqua/sv\?url=([^&]+)""".toRegex()
+
+    override var url: String = urlRegex.find(url)?.groupValues?.get(1) ?:
+    throw InvalidServerException(context.getString(R.string.unknown_error))
+
+    override suspend fun onExtract() {
+
+        val response = OkHttpClient()
+            .newCall(Request.Builder().url(url).build())
+            .await()
+
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(
+            R.string.server_domain_is_down,
+            Properties.SendvidIdentifier))
+
+        url = PatternManager
+            .singleMatch(
+                string =  response.body?.string().toString(),
+                regex =  "<source src=\"(.*?)\""
+            ) ?: throw InvalidServerException(context.getString(
+            R.string.server_requested_resource_was_taken_down,
+            Properties.SendvidIdentifier))
+
+        addDefault()
+    }
+}
