caa1d018dbc6a4e26a2927630bdc189f2efb8de8
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-20 19:48:28 -0500

    new events

--- app/src/main/java/com/ead/project/moongetter/main/MainEvent.kt
+++ app/src/main/java/com/ead/project/moongetter/main/MainEvent.kt
@@ -3,5 +3,8 @@ package com.ead.project.moongetter.main
 import android.content.Context
 
 sealed class MainEvent {
-     class OnNewResult(val context: Context,val url : String) : MainEvent()
+     class OnNewResult(val context: Context,val url : String?=null) : MainEvent()
+     class OnUntilFindNewResult(val context: Context,val urls: List<String>) : MainEvent()
+     class OnNewResults(val context: Context,val urls : List<String> = emptyList()) : MainEvent()
+     class OnSelectedUrl(val url : String) : MainEvent()
 }
