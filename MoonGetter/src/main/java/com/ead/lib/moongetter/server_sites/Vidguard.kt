8102dab9d0f116dc8ceb96046b38ac76376265ca
darkryh <alex_torres-xc@hotmail.com>
2024-09-30 20:20:47 -0500

    pending class

--- MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Vidguard.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Vidguard.kt
@@ -2,13 +2,15 @@ package com.ead.lib.moongetter.server_sites
 
 import android.content.Context
 import com.ead.lib.moongetter.R
+import com.ead.lib.moongetter.core.Pending
 import com.ead.lib.moongetter.core.Properties
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
 
+@Pending
 class Vidguard(context: Context,url: String) : Server(context,url){
 
-    private val  interceptionRegex = """https://[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+/hls_3/[a-zA-Z0-9_\-]+/master\.m3u8\?sig=[a-zA-Z0-9_\-]+&expires=\d+"""".toRegex()
+    //private val  interceptionRegex = """https://[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+/hls_3/[a-zA-Z0-9_\-]+/master\.m3u8\?sig=[a-zA-Z0-9_\-]+&expires=\d+"""".toRegex()
 
     override val isDeprecated: Boolean get() = false
 
