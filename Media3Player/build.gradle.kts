cc255cbb6b953600a94e671550087536b7f6702a
darkryh <alex_torres-xc@hotmail.com>
2024-09-30 20:51:49 -0500

    Updated gradle to Java 17

--- Media3Player/build.gradle.kts
+++ Media3Player/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -59,7 +59,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation ("androidx.media3:media3-exoplayer:1.3.1")
-    implementation ("androidx.media3:media3-ui:1.3.1")
-    implementation ("androidx.media3:media3-exoplayer-hls:1.3.1")
+    implementation (libs.androidx.media3.exoplayer)
+    implementation (libs.androidx.media3.ui)
+    implementation (libs.androidx.media3.exoplayer.hls)
 }
