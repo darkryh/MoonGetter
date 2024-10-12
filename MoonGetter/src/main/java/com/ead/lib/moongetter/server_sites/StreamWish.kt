1f2f8949cb7ad2bdc3d901f1006150655470076f
darkryh <alex_torres-xc@hotmail.com>
2024-10-12 13:51:57 -0500

    server reference using ServerJwPlayer method

--- MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/StreamWish.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/StreamWish.kt
@@ -1,34 +1,9 @@
 package com.ead.lib.moongetter.server_sites
 
 import android.content.Context
-import com.ead.lib.moongetter.R
-import com.ead.lib.moongetter.core.Pending
-import com.ead.lib.moongetter.core.Properties
-import com.ead.lib.moongetter.core.Unstable
-import com.ead.lib.moongetter.core.system.extensions.await
-import com.ead.lib.moongetter.models.Server
-import com.ead.lib.moongetter.models.exceptions.InvalidServerException
-import com.ead.lib.moongetter.utils.PatternManager
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import com.ead.lib.moongetter.models.ServerJwPlayer
 
-@Pending
-@Unstable(reason = "Needs to adapt an api identifying new subdomains")
-class StreamWish(context: Context, url : String) : Server(context,url) {
-
-    override suspend fun onExtract() {
-
-        val response = OkHttpClient()
-            .newCall(Request.Builder().url(url).build())
-            .await()
-
-        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down,Properties.StreamWishIdentifier))
-
-        url = PatternManager.singleMatch(
-            string =  response.body?.string().toString(),
-            regex =  """file:"(https://[^"]+)""""
-        ) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down,Properties.StreamWishIdentifier))
-
-        addDefault()
-    }
+class StreamWish(context: Context, url : String) : ServerJwPlayer(context,url) {
+    override val endingRegex: Regex = """https:\\/\\/mc\\.yandex\\.ru\\/clmap\\/.*""".toRegex()
+    override suspend fun onExtract() = onDefaultJwPlayer()
 }
--- MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Vihide.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Vihide.kt
@@ -1,22 +1,9 @@
 package com.ead.lib.moongetter.server_sites
 
 import android.content.Context
-import com.ead.lib.moongetter.R
-import com.ead.lib.moongetter.core.Properties
-import com.ead.lib.moongetter.models.Server
-import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.models.ServerJwPlayer
 
-class Vihide(context: Context, url: String) : Server(context,url){
-
-    private val interceptionRegex = """https://streamtape\.com/get_video\?id=[\w-]+&expires=\d+&ip=[\w-]+&token=[\w-]+&stream=\d+""".toRegex()
-    private val endingRegex = """https://streamtape\.com/favicon\.ico""".toRegex()
-
-    override suspend fun onExtract() {
-        initializeBrowser(false)
-
-        url = getInterceptionUrl(url,interceptionRegex,endingRegex) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down,Properties.StreamtapeIdentifier))
-        releaseBrowser()
-
-        addDefault()
-    }
+class Vihide(context: Context, url: String) : ServerJwPlayer(context,url){
+    override val endingRegex: Regex = """https:\\/\\/mc\\.yandex\\.ru\\/clmap\\/.*""".toRegex()
+    override suspend fun onExtract() = onDefaultJwPlayer()
 }
