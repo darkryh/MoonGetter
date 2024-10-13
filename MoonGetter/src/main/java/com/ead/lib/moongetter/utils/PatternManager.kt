41c799a33bae16ec90388ef87e802d0b46e536bc
darkryh <alex_torres-xc@hotmail.com>
2024-10-13 00:47:09 -0500

    renamed references and added pairs in multiple matches

--- MoonGetter/src/main/java/com/ead/lib/moongetter/utils/PatternManager.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/utils/PatternManager.kt
@@ -14,15 +14,32 @@ object PatternManager {
         } else null
     }
 
-    fun multipleMatches(string: String, regex: String, groupIndex: Int=1): List<String> {
-        val pattern = Pattern.compile(regex, Pattern.MULTILINE)
+    fun findMultipleMatches(string: String, regex: String, groupIndex: Int=1, patternFlag: Int = Pattern.MULTILINE): List<String> {
+        val pattern = Pattern.compile(regex, patternFlag)
         val matcher = pattern.matcher(string)
         val stringArrayList = ArrayList<String>()
         while (matcher.find()) {
             matcher.group(groupIndex)?.let { stringArrayList.add(it) }
         }
-        if (stringArrayList.isEmpty) throw RuntimeException("null")
+        if (stringArrayList.isEmpty) throw RuntimeException("No matches found.")
         return stringArrayList
+    }
+
+    fun findMultipleMatchesAsPairs(string: String, regex: String): List<Pair<String, String>> {
+        val pattern = Pattern.compile(regex, Pattern.DOTALL)
+        val matcher = pattern.matcher(string)
+        val matches = mutableListOf<Pair<String, String>>()
+
+        while (matcher.find()) {
+            val key = matcher.group(1)
+            val value = matcher.group(2)
+            if (key != null && value != null) {
+                matches.add(Pair(key, value))
+            }
+        }
+
+        if (matches.isEmpty()) throw RuntimeException("No matches found.")
+        return matches
     }
 
     fun match(regex: String, string: String): Boolean {
