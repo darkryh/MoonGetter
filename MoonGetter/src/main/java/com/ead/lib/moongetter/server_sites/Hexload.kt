08962806255f968eb6a277f528f45364e850d7a7
darkryh <alex_torres-xc@hotmail.com>
2024-10-13 00:56:06 -0500

    new server implemented Hexload

---
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/server_sites/Hexload.kt
@@ -0,0 +1,57 @@
+package com.ead.lib.moongetter.server_sites
+
+import android.content.Context
+import com.ead.lib.moongetter.R
+import com.ead.lib.moongetter.core.Properties
+import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.utils.PatternManager
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+
+class Hexload(context: Context,url: String) : Server(context,url) {
+
+    override suspend fun onExtract() {
+        var response = OkHttpClient()
+            .newCall(Request.Builder().url(url).build())
+            .await()
+
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down,Properties.HexloadIdentifier))
+
+
+        val dataPattern = """data:\s*\{\s*(.*?)\s*\}""".toRegex(RegexOption.DOT_MATCHES_ALL)
+        val dataContent = dataPattern.find(response.body?.string().toString())?.groupValues?.get(1)
+
+        val dataParams = PatternManager.findMultipleMatchesAsPairs(
+            string = dataContent.toString(),
+            regex = """(\w+):\s*['"]([^'"]+)['"]"""
+        )
+
+        val formBodyBuilder = FormBody.Builder()
+
+        dataParams.forEach { (key, value) -> formBodyBuilder.add(key, value) }
+
+        val client = OkHttpClient()
+        val formBody = formBodyBuilder.build()
+
+        val request = Request.Builder()
+            .url("https://hexload.com/download")
+            .post(formBody)
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .build()
+
+        response = client.newCall(request).await()
+
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down,Properties.HexloadIdentifier))
+
+        val responseBody = response.body?.string().toString()
+
+        val jsonObject = JSONObject(responseBody)
+        url = jsonObject.getJSONObject("result").getString("url")
+
+        addDefault()
+    }
+}
