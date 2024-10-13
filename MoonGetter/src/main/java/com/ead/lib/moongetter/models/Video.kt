c7cb1e4bd732b8f2b4249e45feff85ed4709f8b3
darkryh <alex_torres-xc@hotmail.com>
2024-10-13 00:54:15 -0500

    updated data class and added constructor option

--- MoonGetter/src/main/java/com/ead/lib/moongetter/models/Video.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/models/Video.kt
@@ -1,8 +1,16 @@
 package com.ead.lib.moongetter.models
 
+import com.ead.lib.moongetter.models.download.Request
+
 data class Video(
     val quality : String?,
-    val url : String,
-    val cookies : Map<String, String> ?= null,
-    val headers : Map<String, String> ?= null
-)
+    val request: Request
+) {
+    constructor(
+        quality: String?,
+        url: String,
+        method: String = "GET",
+        cookies : Map<String, String> ?= null,
+        headers : Map<String, String> ?= null
+    ) : this(quality, Request(url = url, method = method, cookies = cookies, headers = headers))
+}
