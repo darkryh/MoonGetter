52d995a781bda6d6e2f9cccc96953aa1a04476df
darkryh <alex_torres-xc@hotmail.com>
2024-10-16 21:07:24 -0500

    request event replace

--- app/src/main/java/com/ead/project/moongetter/main/MainEvent.kt
+++ app/src/main/java/com/ead/project/moongetter/main/MainEvent.kt
@@ -1,10 +1,11 @@
 package com.ead.project.moongetter.main
 
 import android.content.Context
+import com.ead.lib.moongetter.models.download.Request
 
 sealed class MainEvent {
      class OnNewResult(val context: Context,val url : String?=null) : MainEvent()
      class OnUntilFindNewResult(val context: Context,val urls: List<String>) : MainEvent()
      class OnNewResults(val context: Context,val urls : List<String> = emptyList()) : MainEvent()
-     class OnSelectedUrl(val url : String) : MainEvent()
+     class OnSelectedUrl(val request: Request) : MainEvent()
 }
