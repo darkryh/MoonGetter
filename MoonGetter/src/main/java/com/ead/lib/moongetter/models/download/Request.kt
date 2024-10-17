8a2699f8995718eeee4f5ede455cf2a2fbb7218e
darkryh <alex_torres-xc@hotmail.com>
2024-10-16 21:08:44 -0500

    removed cookies property not useful

--- MoonGetter/src/main/java/com/ead/lib/moongetter/models/download/Request.kt
+++ MoonGetter/src/main/java/com/ead/lib/moongetter/models/download/Request.kt
@@ -3,6 +3,5 @@ package com.ead.lib.moongetter.models.download
 data class Request(
     val url: String,
     val method: String,
-    val cookies : Map<String, String>?,
     val headers : Map<String, String>?,
 )
