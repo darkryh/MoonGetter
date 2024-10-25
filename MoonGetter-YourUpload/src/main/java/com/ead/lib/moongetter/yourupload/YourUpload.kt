def8c972e565f32b2d5cce7bfe68a594049c9651
darkryh <alex_torres-xc@hotmail.com>
2024-10-25 15:15:29 -0500

    YourUpload upgraded

--- MoonGetter-YourUpload/src/main/java/com/ead/lib/moongetter/yourupload/YourUpload.kt
+++ MoonGetter-YourUpload/src/main/java/com/ead/lib/moongetter/yourupload/YourUpload.kt
@@ -1,8 +1,7 @@
-package com.ead.lib.moongetter.server_sites
+package com.ead.lib.moongetter.yourupload
 
 import android.content.Context
 import com.ead.lib.moongetter.R
-import com.ead.lib.moongetter.core.Properties
 import com.ead.lib.moongetter.core.Unstable
 import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Server
@@ -11,10 +10,13 @@ import com.ead.lib.moongetter.models.exceptions.InvalidServerException
 import com.ead.lib.moongetter.utils.PatternManager
 import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
-import okhttp3.Request
 
 @Unstable("server request take too long to respond")
-class YourUpload(context: Context, url : String) : Server(context,url) {
+class YourUpload(
+    context: Context,
+    url : String,
+    headers : HashMap<String,String>
+) : Server(context,url,headers) {
 
     override suspend fun onExtract(): List<Video> {
         val client = OkHttpClient().newBuilder()
@@ -34,28 +36,32 @@ class YourUpload(context: Context, url : String) : Server(context,url) {
             .build()
 
         var response = client
-            .newCall(Request.Builder().url(url).build())
+            .newCall(GET())
             .await()
 
-        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down,Properties.YourUploadIdentifier))
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down, name))
 
         val urlRequest = PatternManager.singleMatch(
-            string =  response.body?.string().toString(),
+            string =  response.body?.string() ?: throw InvalidServerException(context.getString(R.string.server_response_went_wrong, name)),
             regex = """file:\s*'([^']*)'""",
-        ) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down,Properties.YourUploadIdentifier))
+        ) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down, name))
 
-        val request = Request.Builder()
-            .addHeader("Referer",url)
-            .url(urlRequest)
+        response = client.newCall(
+            GET(
+                url = urlRequest,
+                headers = hashMapOf(
+                    "Referer" to url
+                )
+            )
+        ).await()
 
-        response = client.newCall(request.build()).await()
-
-        if (!response.isRedirect) throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down,Properties.YourUploadIdentifier))
+        if (!response.isRedirect) throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down, name))
 
         return listOf(
             Video(
                 quality = DEFAULT,
-                url = response.header("Location") ?: throw InvalidServerException(context.getString(R.string.server_resource_could_not_find_it,Properties.YourUploadIdentifier))
+                url = response.header("Location") ?: throw InvalidServerException(context.getString(
+                    R.string.server_resource_could_not_find_it, name))
             )
         )
     }
