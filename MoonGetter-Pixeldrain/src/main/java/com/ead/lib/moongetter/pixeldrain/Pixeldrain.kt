60f34c9f49cfa9f3268bc881532c5efe2549a7da
darkryh <alex_torres-xc@hotmail.com>
2024-10-25 15:14:03 -0500

    Pixeldrain upgraded

---
+++ MoonGetter-Pixeldrain/src/main/java/com/ead/lib/moongetter/pixeldrain/Pixeldrain.kt
@@ -0,0 +1,38 @@
+package com.ead.lib.moongetter.pixeldrain
+
+import android.content.Context
+import com.ead.lib.moongetter.R
+import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.Video
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.utils.PatternManager
+import okhttp3.OkHttpClient
+
+class Pixeldrain(
+    context: Context,
+    url : String,
+    headers : HashMap<String,String>
+) : Server(context,url,headers) {
+
+    override suspend fun onExtract(): List<Video> {
+        val id = PatternManager.singleMatch(
+            string = url,
+            regex = """/u/([a-zA-Z0-9]+)"""
+        )
+
+        url = "https://pixeldrain.com/api/file/$id?download"
+
+        val response = OkHttpClient()
+            .newCall(GET())
+            .execute()
+
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down, name))
+
+        return listOf(
+            Video(
+                quality = DEFAULT,
+                url = url
+            )
+        )
+    }
+}
