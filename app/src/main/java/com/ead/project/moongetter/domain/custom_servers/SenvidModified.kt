8b981644ca20f1cbbb068b98a3e84bb4b62aff01
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:50:24 -0500

    unused

--- app/src/main/java/com/ead/project/moongetter/domain/custom_servers/SenvidModified.kt
+++ app/src/main/java/com/ead/project/moongetter/domain/custom_servers/SenvidModified.kt
@@ -1,31 +1,29 @@
 package com.ead.project.moongetter.domain.custom_servers
 
 import android.content.Context
-import com.ead.lib.moongetter.R
-import com.ead.lib.moongetter.core.Properties
-import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.Server
-import com.ead.lib.moongetter.models.Video
-import com.ead.lib.moongetter.models.exceptions.InvalidServerException
-import com.ead.lib.moongetter.utils.PatternManager
-import okhttp3.OkHttpClient
-import okhttp3.Request
 
-class SenvidModified(context : Context,url :String) : Server(context,url) {
+class SenvidModified(
+    context : Context,
+    url :String,
+    headers : HashMap<String,String>,
+    configData: Configuration.Data
+) : Server(context,url,headers,configData) {
 
-    private val urlRegex =  """https://custom\.domain\.com/aqua/sv\?url=([^&]+)""".toRegex()
+    /*private val urlRegex =  """https://custom\.domain\.com/aqua/sv\?url=([^&]+)""".toRegex()
 
     override var url: String = urlRegex.find(url)?.groupValues?.get(1) ?:
     throw InvalidServerException(context.getString(R.string.unknown_error))
 
     override suspend fun onExtract(): List<Video> {
         val response = OkHttpClient()
-            .newCall(Request.Builder().url(url).build())
+            .newCall(GETOk())
             .await()
 
         if (!response.isSuccessful) throw InvalidServerException(context.getString(
             R.string.server_domain_is_down,
-            Properties.SendvidIdentifier))
+            name))
 
         return listOf(
             Video(
@@ -35,8 +33,8 @@ class SenvidModified(context : Context,url :String) : Server(context,url) {
                     regex =  "<source src=\"(.*?)\""
                 ) ?: throw InvalidServerException(context.getString(
                     R.string.server_requested_resource_was_taken_down,
-                    Properties.SendvidIdentifier))
+                    name))
             )
         )
-    }
+    }*/
 }
