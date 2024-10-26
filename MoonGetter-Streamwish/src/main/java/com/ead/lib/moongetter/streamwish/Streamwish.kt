dc7ec1b4cbd2d8fe3fc18989e4d2bcdd4efa6a80
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:48:21 -0500

    New upgraded server streamwish

--- MoonGetter-Streamwish/src/main/java/com/ead/lib/moongetter/streamwish/Streamwish.kt
+++ MoonGetter-Streamwish/src/main/java/com/ead/lib/moongetter/streamwish/Streamwish.kt
@@ -1,13 +1,15 @@
 package com.ead.lib.moongetter.streamwish
 
 import android.content.Context
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.ServerJwPlayer
 
 class Streamwish(
     context: Context,
     url : String,
-    headers : HashMap<String,String>
-) : ServerJwPlayer(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : ServerJwPlayer(context,url,headers,configurationData) {
     override val endingRegex: Regex = """https:\\/\\/mc\\.yandex\\.ru\\/clmap\\/.*""".toRegex()
     override suspend fun onExtract() = onDefaultJwPlayer()
 }
