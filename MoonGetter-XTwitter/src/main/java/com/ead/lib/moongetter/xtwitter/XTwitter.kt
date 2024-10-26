2e422a5db4cdebef7b57bbdaf6cea6c862b3c46f
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:46:11 -0500

    New server xtwitter

--- MoonGetter-XTwitter/src/main/java/com/ead/lib/moongetter/xtwitter/XTwitter.kt
+++ MoonGetter-XTwitter/src/main/java/com/ead/lib/moongetter/xtwitter/XTwitter.kt
@@ -3,6 +3,7 @@ package com.ead.lib.moongetter.xtwitter
 import android.content.Context
 import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
@@ -15,13 +16,15 @@ import kotlin.collections.map
 class XTwitter(
     context: Context,
     url: String,
-    headers : HashMap<String,String>
-) : Server(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : Server(context,url,headers,configurationData) {
 
     var targetUrl = "https://twdown.net/download.php"
 
     override suspend fun onExtract(): List<Video> {
         val response = OkHttpClient()
+            .configBuilder()
             .newCall(
                 POST(
                     url = targetUrl,
