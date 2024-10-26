bc99f736df5f4a1e844abae9acddcec0f5f41628
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:47:15 -0500

    New upgraded server google drive

--- MoonGetter-GoogleDrive/src/main/java/com/ead/lib/moongetter/googledrive/GoogleDrive.kt
+++ MoonGetter-GoogleDrive/src/main/java/com/ead/lib/moongetter/googledrive/GoogleDrive.kt
@@ -3,6 +3,7 @@ package com.ead.lib.moongetter.googledrive
 import android.content.Context
 import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
@@ -12,13 +13,15 @@ import okhttp3.OkHttpClient
 class GoogleDrive (
     context: Context,
     url : String,
-    headers : HashMap<String,String>
-) : Server(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : Server(context,url,headers,configurationData) {
 
     override var url: String = fixUrl(url)
 
     override suspend fun onExtract(): List<Video> {
         val response = OkHttpClient()
+            .configBuilder()
             .newCall(GET())
             .await()
 
