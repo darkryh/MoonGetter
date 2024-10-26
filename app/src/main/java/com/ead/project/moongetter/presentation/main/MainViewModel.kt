03eccd81e61e15b23381165f724a5e05bee71f49
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:56:02 -0500

    example viewmodel

---
+++ app/src/main/java/com/ead/project/moongetter/presentation/main/MainViewModel.kt
@@ -0,0 +1,166 @@
+package com.ead.project.moongetter.presentation.main
+
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ead.lib.moongetter.MoonGetter
+import com.ead.lib.moongetter.doodstream.factory.DoodstreamFactory
+import com.ead.lib.moongetter.facebook.factory.FacebookFactory
+import com.ead.lib.moongetter.filemoon.factory.FilemoonFactory
+import com.ead.lib.moongetter.fireload.factory.FireloadFactory
+import com.ead.lib.moongetter.gofile.factory.GofileFactory
+import com.ead.lib.moongetter.goodstream.factory.GoodStreamFactory
+import com.ead.lib.moongetter.googledrive.factory.GoogleDriveFactory
+import com.ead.lib.moongetter.hexload.factory.HexloadFactory
+import com.ead.lib.moongetter.lulustream.factory.LulustreamFactory
+import com.ead.lib.moongetter.mediafire.factory.MediafireFactory
+import com.ead.lib.moongetter.mixdrop.factory.MixdropFactory
+import com.ead.lib.moongetter.models.builder.Engine
+import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.Video
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.models.Request
+import com.ead.lib.moongetter.mp4upload.factory.Mp4UploadFactory
+import com.ead.lib.moongetter.okru.factory.OkruFactory
+import com.ead.lib.moongetter.onecloudfile.factory.OneCloudFileFactory
+import com.ead.lib.moongetter.pixeldrain.factory.PixeldrainFactory
+import com.ead.lib.moongetter.senvid.factory.SenvidFactory
+import com.ead.lib.moongetter.streamtape.factory.StreamtapeFactory
+import com.ead.lib.moongetter.streamwish.factory.StreamwishFactory
+import com.ead.lib.moongetter.uqload.factory.UqloadFactory
+import com.ead.lib.moongetter.vidguard.factory.VidguardFactory
+import com.ead.lib.moongetter.vihide.factory.VihideFactory
+import com.ead.lib.moongetter.voe.factory.VoeFactory
+import com.ead.lib.moongetter.xtwitter.factory.XTwitterFactory
+import com.ead.lib.moongetter.yourupload.factory.YourUploadFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+class MainViewModel : ViewModel() {
+
+    private val _messageResult : MutableState<List<Video>> = mutableStateOf(emptyList())
+    val messageResult : State<List<Video>> = _messageResult
+
+    private val _eventFlow : MutableSharedFlow<UiEvent> = MutableSharedFlow()
+    val eventFlow : SharedFlow<UiEvent> = _eventFlow
+
+    private val _mediaUrlSelected : MutableState<Request?> = mutableStateOf(null)
+    val mediaUrlSelected : State<Request?> = _mediaUrlSelected
+
+    private val engine : Engine = Engine.Builder()
+        .onCore(
+            engines = arrayOf(
+                DoodstreamFactory,
+                FacebookFactory,
+                FilemoonFactory,
+                FireloadFactory,
+                GofileFactory,
+                GoodStreamFactory,
+                GoogleDriveFactory,
+                HexloadFactory,
+                LulustreamFactory,
+                MediafireFactory,
+                MixdropFactory,
+                Mp4UploadFactory,
+                OkruFactory,
+                OneCloudFileFactory,
+                PixeldrainFactory,
+                SenvidFactory,
+                StreamtapeFactory,
+                StreamwishFactory,
+                UqloadFactory,
+                VidguardFactory,
+                VihideFactory,
+                VoeFactory,
+                XTwitterFactory,
+                YourUploadFactory
+            )
+        )
+        .build()
+
+    fun onEvent(event: MainEvent) {
+        viewModelScope.launch (Dispatchers.IO) {
+            try {
+
+                when(event) {
+                    is MainEvent.OnNewResult -> {
+
+                        val serversResults : Server? = MoonGetter
+                            .initialize(context = event.context)
+                            .setHeaders(
+                                mapOf(
+                                    "User-Agent" to "Mozilla/5.0"
+                                )
+                            )
+                            .setTimeout(0)
+                            .setEngine(engine)
+                            .get(event.url ?: return@launch)
+
+                        _messageResult.value = serversResults?.videos ?: emptyList()
+                    }
+
+                    is MainEvent.OnUntilFindNewResult -> {
+
+                        val serversResults : Server? = MoonGetter
+                            .initialize(context = event.context)
+                            .setHeaders(
+                                mapOf(
+                                    "User-Agent" to "Mozilla/5.0"
+                                )
+                            )
+                            .setTimeout(12000)
+                            .setEngine(engine)
+                            .getUntilFindResource(event.urls)
+
+                        _messageResult.value = serversResults?.videos ?: emptyList()
+                    }
+
+                    is MainEvent.OnNewResults -> {
+
+                        val serversResults : List<Server> = MoonGetter
+                            .initialize(context = event.context)
+                            .setHeaders(
+                                mapOf(
+                                    "User-Agent" to "Mozilla/5.0"
+                                )
+                            )
+                            .setTimeout(12000)
+                            .setEngine(engine)
+                            .get(event.urls)
+
+                        _messageResult.value = serversResults
+                            .flatMap { it.videos }
+                    }
+
+                    is MainEvent.OnSelectedUrl -> {
+                        _mediaUrlSelected.value = event.request
+                    }
+                }
+
+                if (messageResult.value.isNotEmpty() && event !is MainEvent.OnSelectedUrl) {
+                   _mediaUrlSelected.value = messageResult.value.first().request
+                }
+
+            } catch (e : InvalidServerException) {
+                _eventFlow.emit(UiEvent.ShowSnackBar(message = e.message ?: "unknown error"))
+            }
+            catch (e : IOException) {
+                _eventFlow.emit(UiEvent.ShowSnackBar(message = e.message ?: "unknown error"))
+            }
+            catch (e : RuntimeException) {
+                _eventFlow.emit(UiEvent.ShowSnackBar(message = e.message ?: "unknown error"))
+            }
+        }
+
+    }
+
+    sealed class UiEvent {
+        data class ShowSnackBar(val message : String,val actionLabel : String?=null ,val duration : SnackbarDuration = SnackbarDuration.Short) : UiEvent()
+    }
+}
