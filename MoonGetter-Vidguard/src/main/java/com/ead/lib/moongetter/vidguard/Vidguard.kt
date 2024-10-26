c5887d91e302f4872d1191e5d4b3d57e401ea6f3
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:48:29 -0500

    New upgraded server vidguard

--- MoonGetter-Vidguard/src/main/java/com/ead/lib/moongetter/vidguard/Vidguard.kt
+++ MoonGetter-Vidguard/src/main/java/com/ead/lib/moongetter/vidguard/Vidguard.kt
@@ -1,13 +1,15 @@
 package com.ead.lib.moongetter.vidguard
 
 import android.content.Context
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.ServerJwPlayer
 
 class Vidguard(
     context: Context,
     url: String,
-    headers : HashMap<String,String>
-) : ServerJwPlayer(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : ServerJwPlayer(context,url,headers,configurationData) {
     override val endingRegex: Regex = """.*/favicon\.ico$""".toRegex()
     override suspend fun onExtract() = onDefaultJwPlayer()
 }
