240b321540df1d6aad450b7ecd97537eedde941a
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:45:59 -0500

    New server facebook

--- MoonGetter-Facebook/src/main/java/com/ead/lib/moongetter/facebook/Facebook.kt
+++ MoonGetter-Facebook/src/main/java/com/ead/lib/moongetter/facebook/Facebook.kt
@@ -3,6 +3,7 @@ package com.ead.lib.moongetter.facebook
 import android.content.Context
 import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
@@ -13,13 +14,15 @@ import okhttp3.OkHttpClient
 class Facebook(
     context: Context,
     url: String,
-    headers : HashMap<String,String>
-) : Server(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : Server(context,url,headers,configurationData) {
 
     var targetUrl = "https://fdown.net/download.php"
 
     override suspend fun onExtract() : List<Video> {
         val response = OkHttpClient()
+            .configBuilder()
             .newCall(
                 POST(
                     url = targetUrl,
