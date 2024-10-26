f6b4274c52144a31956a2980efccb83422cc353f
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:48:11 -0500

    New upgraded server streamtape

--- MoonGetter-Streamtape/src/main/java/com/ead/lib/moongetter/streamtape/Streamtape.kt
+++ MoonGetter-Streamtape/src/main/java/com/ead/lib/moongetter/streamtape/Streamtape.kt
@@ -1,13 +1,15 @@
 package com.ead.lib.moongetter.streamtape
 
 import android.content.Context
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.ServerJwPlayer
 
 class Streamtape(
     context: Context,
     url : String,
-    headers : HashMap<String,String>
-) : ServerJwPlayer(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : ServerJwPlayer(context,url,headers,configurationData) {
     override val interceptionRegex = """https://streamtape\.com/get_video\?id=[\w-]+&expires=\d+&ip=[\w-]+&token=[\w-]+&stream=\d+""".toRegex()
     override val endingRegex = """https://streamtape\.com/favicon\.ico""".toRegex()
     override fun scriptLoader(): String? = null
