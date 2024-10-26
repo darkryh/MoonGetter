f6724b8baaf8510af68fa87b99425d27a5c41da6
darkryh <alex_torres-xc@hotmail.com>
2024-10-26 14:47:35 -0500

    New upgraded server mediafire

--- MoonGetter-Mediafire/src/main/java/com/ead/lib/moongetter/mediafire/Mediafire.kt
+++ MoonGetter-Mediafire/src/main/java/com/ead/lib/moongetter/mediafire/Mediafire.kt
@@ -3,6 +3,7 @@ package com.ead.lib.moongetter.mediafire
 import android.content.Context
 import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.system.extensions.await
+import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
@@ -12,11 +13,13 @@ import okhttp3.OkHttpClient
 class Mediafire(
     context: Context,
     url : String,
-    headers : HashMap<String,String>
-) : Server(context,url,headers) {
+    headers : HashMap<String,String>,
+    configurationData: Configuration.Data
+) : Server(context,url,headers,configurationData) {
 
     override suspend fun onExtract(): List<Video> {
         val response = OkHttpClient()
+            .configBuilder()
             .newCall(GET())
             .await()
 
