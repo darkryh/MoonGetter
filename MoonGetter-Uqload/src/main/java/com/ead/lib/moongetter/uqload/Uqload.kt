f59d12557df47b0eaae6b446621b837285f215f6
darkryh <alex_torres-xc@hotmail.com>
2024-10-29 11:29:02 -0500

    New Server Uqload

--- MoonGetter-Uqload/src/main/java/com/ead/lib/moongetter/uqload/Uqload.kt
+++ MoonGetter-Uqload/src/main/java/com/ead/lib/moongetter/uqload/Uqload.kt
@@ -2,51 +2,50 @@ package com.ead.lib.moongetter.uqload
 
 import android.content.Context
 import com.ead.lib.moongetter.R
-import com.ead.lib.moongetter.core.Pending
-import com.ead.lib.moongetter.core.Unstable
+import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Configuration
-import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.Request
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.models.ServerUCR
 import com.ead.lib.moongetter.utils.PatternManager
 import okhttp3.OkHttpClient
 
-@Pending
-@Unstable("There is some validations probably in the player side to accept the request")
 class Uqload(
     context: Context,
     url: String,
     headers : HashMap<String,String>,
     configurationData: Configuration.Data
-) : Server(context,url,headers,configurationData) {
+) : ServerUCR(context,url,headers,configurationData) {
 
-    override val isDeprecated: Boolean = true
+    override val headers: HashMap<String, String> = headers.also {
+        it["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        it["Referer"] = url
+        it["Origin"] = url
+    }
 
     override suspend fun onExtract() : List<Video> {
-        val response = OkHttpClient()
+        var response = OkHttpClient()
             .configBuilder()
             .newCall(GET())
-            .execute()
+            .await()
 
-        if (!response.isSuccessful) throw throw InvalidServerException(context.getString(R.string.server_domain_is_down, name))
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down, name))
 
         val body = response.body?.string() ?: throw InvalidServerException(context.getString(R.string.server_response_went_wrong, name))
-
-
-        val headers = mapOf(
-            "Referer" to "https://uqload.ws/",
-        )
-
-        url = PatternManager.singleMatch(
-            string = body,
-            regex = "sources:\\s*\\[\"(http[^\\\"]+)"
-        )?.takeIf { it.startsWith("http") } ?: throw InvalidServerException(context.getString(R.string.server_resource_could_not_find_it, name))
 
         return listOf(
             Video(
                 quality = DEFAULT,
-                url = url,
-                headers = headers
+                request = Request(
+                    url = PatternManager.singleMatch(
+                        string = body,
+                        regex = """sources:\s*\[\s*"(https?://[^"]+)"\s*\]"""
+                    )?.takeIf { it.startsWith("http") } ?: throw InvalidServerException(context.getString(R.string.server_resource_could_not_find_it, name))
+                    ,
+                    method = "GET",
+                    headers = headers
+                )
             )
         )
     }
