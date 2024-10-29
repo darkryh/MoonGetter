9ccf1af5d6e485918a528c1198683dfe6ce4a9d3
darkryh <alex_torres-xc@hotmail.com>
2024-10-29 11:27:57 -0500

    testing Lulustream

--- MoonGetter-Lulustream/src/main/java/com/ead/lib/moongetter/lulustream/Lulustream.kt
+++ MoonGetter-Lulustream/src/main/java/com/ead/lib/moongetter/lulustream/Lulustream.kt
@@ -1,10 +1,17 @@
 package com.ead.lib.moongetter.lulustream
 
 import android.content.Context
+import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.Pending
 import com.ead.lib.moongetter.core.Unstable
+import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Configuration
-import com.ead.lib.moongetter.models.ServerJwPlayer
+import com.ead.lib.moongetter.models.Request
+import com.ead.lib.moongetter.models.Server
+import com.ead.lib.moongetter.models.Video
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.lib.moongetter.utils.PatternManager
+import okhttp3.OkHttpClient
 
 @Pending
 @Unstable(reason = "Needs to validate requests headers")
@@ -13,7 +20,40 @@ class Lulustream(
     url : String,
     headers : HashMap<String,String>,
     configurationData: Configuration.Data
-) : ServerJwPlayer(context,url,headers,configurationData) {
+) : Server(context,url,headers,configurationData) {
+
     override val isDeprecated: Boolean = true
-    override suspend fun onExtract() = onDefaultJwPlayer()
+
+    override val headers: HashMap<String, String> = headers.also {
+        it["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+        it["Referer"] = "https://luluvdo.com"
+        it["Origin"] = "https://luluvdo.com"
+    }
+
+    override suspend fun onExtract() : List<Video> {
+        var response = OkHttpClient()
+            .configBuilder()
+            .newCall(GET())
+            .await()
+
+        if (!response.isSuccessful) throw InvalidServerException(context.getString(com.ead.lib.moongetter.R.string.server_domain_is_down, name))
+
+        val body = response.body?.string() ?: throw InvalidServerException(context.getString(com.ead.lib.moongetter.R.string.server_response_went_wrong, name))
+
+        return listOf(
+            Video(
+                quality = DEFAULT,
+                request = Request(
+                    url = PatternManager.singleMatch(
+                        string = body,
+                        regex = """sources:\s*\[\s*\{file:\s*"(https?://[^"]+)"""
+                    )?.takeIf { it.startsWith("http") } ?: throw InvalidServerException(context.getString(
+                        R.string.server_resource_could_not_find_it, name))
+                    ,
+                    method = "GET",
+                    headers = headers
+                )
+            )
+        )
+    }
 }
