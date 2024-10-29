4f13593edd877a3c63df074058cd2f91f0625309
darkryh <alex_torres-xc@hotmail.com>
2024-10-29 11:29:22 -0500

    strings

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/system/extensions/String.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/system/extensions/String.kt
@@ -3,10 +3,9 @@ package com.ead.lib.moongetter.core.system.extensions
 fun String.delete(string: String) : String =
     this.replace(string,"")
 
-/*
 fun String.toHtml() : String =
     replace("\\u003C","<")
         .lineSeparator()
         .replace("\\\"","\"")
         .takeIf { it.isNotEmpty() } ?: "null"
-        .trim().removeSurrounding("\"")*/
+        .trim().removeSurrounding("\"")
