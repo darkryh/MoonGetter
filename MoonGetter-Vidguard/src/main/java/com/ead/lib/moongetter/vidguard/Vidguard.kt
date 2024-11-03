d99ecf95941e3deabffef5c946edd4370791ad74
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 21:07:13 -0500

    updated constructor

--- MoonGetter-Vidguard/src/main/java/com/ead/lib/moongetter/vidguard/Vidguard.kt
+++ MoonGetter-Vidguard/src/main/java/com/ead/lib/moongetter/vidguard/Vidguard.kt
@@ -2,14 +2,16 @@ package com.ead.lib.moongetter.vidguard
 
 import android.content.Context
 import com.ead.lib.moongetter.models.Configuration
-import com.ead.lib.moongetter.models.ServerJwPlayer
+import com.ead.lib.moongetter.robot.ServerJwPlayer
+import okhttp3.OkHttpClient
 
 class Vidguard(
     context: Context,
     url: String,
+    client: OkHttpClient,
     headers : HashMap<String,String>,
     configurationData: Configuration.Data
-) : ServerJwPlayer(context,url,headers,configurationData) {
+) : ServerJwPlayer(context,url,client,headers,configurationData) {
     override val endingRegex: Regex = """.*/favicon\.ico$""".toRegex()
     override suspend fun onExtract() = onDefaultJwPlayer()
 }
