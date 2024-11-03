94db673394d55a048271038edc8dd1c4ac944652
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 20:56:58 -0500

    test MoonGetter

--- MoonGetter-Core/src/test/java/com/ead/lib/moongetter/MoonGetterTest.kt
+++ MoonGetter-Core/src/test/java/com/ead/lib/moongetter/MoonGetterTest.kt
@@ -2,26 +2,35 @@ package com.ead.lib.moongetter
 
 import android.content.Context
 import com.ead.lib.moongetter.models.builder.Engine
+import com.ead.lib.moongetter.models.builder.Factory
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 
 class MoonGetterTest {
 
-    private lateinit var moonGetterEngine : Engine.Builder
+    private lateinit var moonGetterFactory : Factory.Builder
     private lateinit var context: Context
+
+    private val fakeEngine = Engine.Builder().build()
 
     @Before
     fun setUp() {
-        /*context = mockk(relaxed = true)
-        moonGetterRequest = MoonGetter.initialize(context)*/
+        context = mockk(relaxed = true)
+        moonGetterFactory = MoonGetter
+            .initialize(context)
+            .setEngine(fakeEngine)
     }
 
-    /*@Test(expected = InvalidServerException::class)
+    @Test(expected = InvalidServerException::class)
     fun `validating that when url is empty, an exception is thrown`() {
-        runBlocking { moonGetterRequest.connect("").get() }
+        runBlocking { moonGetterFactory.get("") }
     }
 
     @Test(expected = InvalidServerException::class)
     fun `validating that when urls is empty, an exception is thrown`() {
-        runBlocking { moonGetterRequest.connect(emptyList()).getList() }
-    }*/
+        runBlocking { moonGetterFactory.get(emptyList()) }
+    }
 }
