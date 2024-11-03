b11954b87232aba83978106813d55343478078e0
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 21:00:32 -0500

    Updated constructor reference from server

--- MoonGetter-Voe/src/main/java/com/ead/lib/moongetter/voe/Voe.kt
+++ MoonGetter-Voe/src/main/java/com/ead/lib/moongetter/voe/Voe.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalEncodingApi::class)
+
 package com.ead.lib.moongetter.voe
 
 import android.content.Context
@@ -8,15 +10,17 @@ import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
 import com.ead.lib.moongetter.utils.PatternManager
-import com.ead.lib.moongetter.utils.decoder
 import okhttp3.OkHttpClient
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 class Voe(
     context: Context,
     url : String,
+    client: OkHttpClient,
     headers : HashMap<String,String>,
-    configurationData: Configuration.Data
-) : Server(context,url,headers,configurationData) {
+    configData : Configuration.Data,
+) : Server(context, url, client, headers, configData) {
 
     override suspend fun onExtract(): List<Video> {
         var response = OkHttpClient()
@@ -41,12 +45,12 @@ class Voe(
         return listOf(
             Video(
                 quality = DEFAULT,
-                url = decoder(
+                url = Base64.decode(
                     PatternManager.singleMatch(
                         string = response.body?.string() ?: throw InvalidServerException(context.getString(R.string.server_response_went_wrong, name)),
                         regex = """'hls':\s*'([^']+)"""
                     ) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down, name))
-                )
+                ).toString(Charsets.UTF_8)
             )
         )
     }
