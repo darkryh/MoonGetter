13bcba4b3cd7efc955d3616deede4e4ddae6ebc1
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 20:49:53 -0500

    new annotations

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/Annotations.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/Annotations.kt
@@ -1,9 +1,31 @@
+@file:Suppress("UNUSED")
+
 package com.ead.lib.moongetter.core
 
 @Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.SOURCE )
 annotation class Pending
 
 @Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.SOURCE )
 annotation class Unstable(val reason : String)
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Requires(val libraryName: String)
+
+@RequiresOptIn(
+    level = RequiresOptIn.Level.WARNING,
+    message = "This function is experimental some ExperimentalServer are not compatible with it"
+)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+annotation class ExperimentalFeature
+
+@RequiresOptIn(
+    level = RequiresOptIn.Level.WARNING,
+    message = "This server is experimental his basic functionality is not guaranteed"
+)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+annotation class ExperimentalServer
