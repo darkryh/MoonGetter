07f2f0062a62f1a968e4a481a86aa8373d2e3665
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 20:51:21 -0500

    Updated constructor factory

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/CoreExtensions.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/CoreExtensions.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import com.ead.lib.moongetter.models.Configuration
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.utils.PatternManager
+import okhttp3.OkHttpClient
 
 internal fun Array<Server.Factory>.onFactory(
     context: Context,
     url: String,
     headers: HashMap<String, String>,
-    configData: Configuration.Data
+    configData: Configuration.Data,
+    client: OkHttpClient
 ): Server? {
     return singleOrNull { expectedServerFactory ->
         PatternManager
@@ -22,10 +24,11 @@ internal fun Array<Server.Factory>.onFactory(
             val constructor = `class`.getDeclaredConstructor(
                 Context::class.java,
                 String::class.java,
+                OkHttpClient::class.java,
                 HashMap::class.java,
                 Configuration.Data::class.java
             )
-            constructor.newInstance(context, url, headers, configData)
+            constructor.newInstance(context, url, client, headers, configData)
         }
     }
 }
