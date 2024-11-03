dd519c2926a8e27ce339464297320677b500521c
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 21:09:46 -0500

    added const for testing

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/utils/Values.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/utils/Values.kt
@@ -1,5 +1,20 @@
+@file:Suppress("RestrictedApi")
+
 package com.ead.lib.moongetter.utils
+
+import androidx.annotation.VisibleForTesting
 
 object Values {
     const val DEBUG_ERROR = "MOON_ERROR"
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    var targetUrl : String? = null
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    var targetUrl2 : String? = null
+
+    fun restoreValues() {
+        targetUrl = null
+        targetUrl2 = null
+    }
 }
