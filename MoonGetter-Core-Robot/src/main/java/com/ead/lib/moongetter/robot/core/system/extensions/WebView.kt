1ecf87e5a5ef83cedebb7f9da43c30722338901d
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 21:03:53 -0500

    moved package into robot

--- MoonGetter-Core-Robot/src/main/java/com/ead/lib/moongetter/robot/core/system/extensions/WebView.kt
+++ MoonGetter-Core-Robot/src/main/java/com/ead/lib/moongetter/robot/core/system/extensions/WebView.kt
@@ -1,6 +1,7 @@
-package com.ead.lib.moongetter.core.system.extensions
+package com.ead.lib.moongetter.robot.core.system.extensions
 
 import android.webkit.WebView
+import com.ead.lib.moongetter.core.system.extensions.delete
 
 fun WebView.getSourceCode(callback: (String) -> Unit) {
     evaluateJavascript(
@@ -21,6 +22,14 @@ fun WebView.getSourceCode(callback: (String) -> Unit) {
         callback(result)
     }
 }
+
+fun String.toHtml() : String =
+    replace("\\u003C","<")
+        .lineSeparator()
+        .replace("\\\"","\"")
+        .takeIf { it.isNotEmpty() } ?: "null"
+        .trim().removeSurrounding("\"")
+
 
 fun String.lineSeparator(): String {
     return this.delete("\\n") + System.lineSeparator()
