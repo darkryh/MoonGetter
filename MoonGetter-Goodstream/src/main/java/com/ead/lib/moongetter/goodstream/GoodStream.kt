2f6efc8b21f81eec19dc9576845d113179337e93
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 21:06:16 -0500

    testing server

--- MoonGetter-Goodstream/src/main/java/com/ead/lib/moongetter/goodstream/GoodStream.kt
+++ MoonGetter-Goodstream/src/main/java/com/ead/lib/moongetter/goodstream/GoodStream.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Configuration
+import com.ead.lib.moongetter.models.Request
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
-import com.ead.lib.moongetter.models.Request
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
 import com.ead.lib.moongetter.utils.PatternManager
 import okhttp3.OkHttpClient
@@ -14,14 +14,15 @@ import okhttp3.OkHttpClient
 class GoodStream(
     context: Context,
     url : String,
+    client: OkHttpClient,
     headers : HashMap<String,String>,
-    configurationData: Configuration.Data
-) : Server(context,url,headers,configurationData) {
+    configData : Configuration.Data,
+) : Server(context, url, client, headers, configData) {
 
     override val isDeprecated: Boolean = true
 
     override suspend fun onExtract(): List<Video> {
-        val response = OkHttpClient()
+        val response = client
             .configBuilder()
             .newCall(GET())
             .await()
@@ -37,11 +38,7 @@ class GoodStream(
                         regex = """file:\s*"(https?://[^\s"]+)""""
                     ) ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down, name)),
                     method = "GET",
-                    headers = mapOf(
-                        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
-                        "Referer" to "https://goodstream.one",
-                        "Origin" to "https://goodstream.one"
-                    )
+                    headers = response.headers.toMap()
                 )
             )
         )
