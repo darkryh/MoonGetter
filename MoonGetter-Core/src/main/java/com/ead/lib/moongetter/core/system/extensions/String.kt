10cba6263d4084b49b5b51c5cbaa2e9df23e5fca
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 21:10:01 -0500

    updated func extensions

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/system/extensions/String.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/core/system/extensions/String.kt
@@ -1,11 +1,17 @@
 package com.ead.lib.moongetter.core.system.extensions
 
+import com.ead.lib.moongetter.utils.PatternManager
+
 fun String.delete(string: String) : String =
     this.replace(string,"")
 
-fun String.toHtml() : String =
-    replace("\\u003C","<")
-        .lineSeparator()
-        .replace("\\\"","\"")
-        .takeIf { it.isNotEmpty() } ?: "null"
-        .trim().removeSurrounding("\"")
+fun String.replaceDomainWith(domain: String) : String? {
+    val domainToReplace = PatternManager.singleMatch(
+        string = this,
+        regex = """https?://([a-zA-Z0-9\-]+\.[a-zA-Z]{2,})(/[^\s]*)?"""
+    )
+
+    return domainToReplace?.let {
+        this.replace(domainToReplace, domain)
+    }
+}
