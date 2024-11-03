6dacb3983eaae16ce3a5b0cd491e4e3c2bb71074
darkryh <alex_torres-xc@hotmail.com>
2024-11-02 20:49:19 -0500

    testing server

--- MoonGetter-Abyss/src/main/java/com/ead/lib/moongetter/abyss/Abyss.kt
+++ MoonGetter-Abyss/src/main/java/com/ead/lib/moongetter/abyss/Abyss.kt
@@ -1,8 +1,8 @@
+@file:OptIn(ExperimentalEncodingApi::class)
+
 package com.ead.lib.moongetter.abyss
 
 import android.content.Context
-import android.os.Environment
-import android.util.Base64
 import com.ead.lib.moongetter.R
 import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Configuration
@@ -10,17 +10,17 @@ import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
 import com.ead.lib.moongetter.utils.PatternManager
-import com.ead.lib.moongetter.utils.decoder
 import okhttp3.OkHttpClient
-import java.io.File
-import java.io.FileOutputStream
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 class Abyss(
     context: Context,
     url : String,
+    client: OkHttpClient,
     headers : HashMap<String,String>,
-    configurationData: Configuration.Data
-) : Server(context,url,headers,configurationData) {
+    configData : Configuration.Data,
+) : Server(context, url, client, headers, configData) {
 
     override val isDeprecated: Boolean = true
 
@@ -32,15 +32,15 @@ class Abyss(
 
         if (!response.isSuccessful) throw InvalidServerException(context.getString(R.string.server_domain_is_down, name))
 
-        val dataInBase64 = decoder(
+        val dataInBase64 = Base64.decode(
             PatternManager.singleMatch(
                 string = response.body?.string() ?: throw InvalidServerException(context.getString(
                     R.string.server_response_went_wrong, name)),
                 regex = """atob\("([^"]+)""""
             )?.removeSuffix("_") ?: throw InvalidServerException(context.getString(R.string.server_requested_resource_was_taken_down, name))
-        )
+        ).toString(Charsets.UTF_8)
 
-        saveFile(decodeBase64(dataInBase64))
+        //saveFile(decodeBase64(dataInBase64))
 
         return listOf(
             Video(
@@ -50,7 +50,7 @@ class Abyss(
         )
     }
 
-    fun decodeBase64(base64String: String): ByteArray {
+    /*fun decodeBase64(base64String: String): ByteArray {
         return Base64.decode(base64String, Base64.DEFAULT)
     }
 
@@ -58,5 +58,5 @@ class Abyss(
         val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         val file = File(downloadDir, "video${System.currentTimeMillis()}.mp4")
         FileOutputStream(file).use { it.write(bytes) }
-    }
+    }*/
 }
