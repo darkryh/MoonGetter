7dbf09fefc07de293521c6ea7fb46f8f201eafef
darkryh <alex_torres-xc@hotmail.com>
2024-11-14 11:44:56 -0500

    textfield compose

--- app/src/main/java/com/ead/project/moongetter/presentation/main/components/TextField.kt
+++ app/src/main/java/com/ead/project/moongetter/presentation/main/components/TextField.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
