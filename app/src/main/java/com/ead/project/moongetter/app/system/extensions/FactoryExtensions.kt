2039967fc7c006653fc97a06d2b9fbd9a99f2fde
darkryh <alex_torres-xc@hotmail.com>
2024-11-14 11:44:10 -0500

    extensions class using as a base MoonGetter to handle throwing

---
+++ app/src/main/java/com/ead/project/moongetter/app/system/extensions/FactoryExtensions.kt
@@ -0,0 +1,172 @@
+package com.ead.project.moongetter.app.system.extensions
+
+import com.ead.lib.moongetter.core.ExperimentalFeature
+import com.ead.lib.moongetter.models.Error
+import com.ead.lib.moongetter.models.builder.Factory
+import com.ead.lib.moongetter.models.exceptions.InvalidServerException
+import com.ead.project.moongetter.app.network.util.MoonGetterError
+import com.ead.project.moongetter.app.network.util.Result
+import kotlinx.io.IOException
+
+suspend inline fun <reified T> Factory.Builder.onGetResult(url : String) : Result<T,MoonGetterError> {
+    return try {
+        val  result = get(url) ?: return Result.Error(MoonGetterError.UNKNOWN)
+        Result.Success(
+            result as T
+        )
+    }
+    catch (e : InvalidServerException) {
+        return when(e.error) {
+            Error.INVALID_URL_PARAMETER -> {
+                Result.Error(MoonGetterError.UNKNOWN)
+            }
+            Error.INVALID_PARAMETERS -> {
+                Result.Error(MoonGetterError.INVALID_PARAMETERS_IN_BUILDER)
+            }
+            Error.NO_PARAMETERS_TO_WORK -> {
+                Result.Error(MoonGetterError.NO_PARAMETERS_TO_WORK)
+            }
+            Error.EMPTY_OR_NULL_RESPONSE -> {
+                Result.Error(MoonGetterError.SERVERS_RESPONSE_WENT_WRONG)
+            }
+            Error.EXPECTED_RESPONSE_NOT_FOUND -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            Error.EXPECTED_PACKED_RESPONSE_NOT_FOUND -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            Error.UNSUCCESSFUL_RESPONSE -> {
+                return when (e.code ?: return Result.Error(MoonGetterError.UNKNOWN)) {
+                    401 -> Result.Error(MoonGetterError.UNAUTHORIZED)
+                    408 -> Result.Error(MoonGetterError.REQUEST_TIMEOUT)
+                    409 -> Result.Error(MoonGetterError.CONFLICT)
+                    413 -> Result.Error(MoonGetterError.PAYLOAD_TOO_LARGE)
+                    429 -> Result.Error(MoonGetterError.TOO_MANY_REQUESTS)
+                    in 500..599 -> Result.Error(MoonGetterError.SERVER_ERROR)
+                    else -> Result.Error(MoonGetterError.UNKNOWN)
+                }
+            }
+            Error.RESOURCE_TAKEN_DOWN -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            else -> {
+                Result.Error(MoonGetterError.UNKNOWN)
+            }
+        }
+    }
+    catch (_ : IOException) {
+        Result.Error(MoonGetterError.NO_INTERNET)
+    }
+    catch (_ : IllegalArgumentException) {
+        Result.Error(MoonGetterError.UNKNOWN)
+    }
+}
+
+suspend inline fun <reified T> Factory.Builder.onGetUntilFindResult(urls : List<String>) : Result<T,MoonGetterError> {
+    return try {
+        val  result = getUntilFindResource(urls) ?: return Result.Error(MoonGetterError.UNKNOWN)
+        Result.Success(
+            result as T
+        )
+    }
+    catch (e : InvalidServerException) {
+        return when(e.error) {
+            Error.INVALID_URL_PARAMETER -> {
+                Result.Error(MoonGetterError.UNKNOWN)
+            }
+            Error.INVALID_PARAMETERS -> {
+                Result.Error(MoonGetterError.INVALID_PARAMETERS_IN_BUILDER)
+            }
+            Error.NO_PARAMETERS_TO_WORK -> {
+                Result.Error(MoonGetterError.NO_PARAMETERS_TO_WORK)
+            }
+            Error.EMPTY_OR_NULL_RESPONSE -> {
+                Result.Error(MoonGetterError.SERVERS_RESPONSE_WENT_WRONG)
+            }
+            Error.EXPECTED_RESPONSE_NOT_FOUND -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            Error.EXPECTED_PACKED_RESPONSE_NOT_FOUND -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            Error.UNSUCCESSFUL_RESPONSE -> {
+                return when (e.code ?: return Result.Error(MoonGetterError.UNKNOWN)) {
+                    401 -> Result.Error(MoonGetterError.UNAUTHORIZED)
+                    408 -> Result.Error(MoonGetterError.REQUEST_TIMEOUT)
+                    409 -> Result.Error(MoonGetterError.CONFLICT)
+                    413 -> Result.Error(MoonGetterError.PAYLOAD_TOO_LARGE)
+                    429 -> Result.Error(MoonGetterError.TOO_MANY_REQUESTS)
+                    in 500..599 -> Result.Error(MoonGetterError.SERVER_ERROR)
+                    else -> Result.Error(MoonGetterError.UNKNOWN)
+                }
+            }
+            Error.RESOURCE_TAKEN_DOWN -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            else -> {
+                Result.Error(MoonGetterError.UNKNOWN)
+            }
+        }
+    }
+    catch (_ : IOException) {
+        Result.Error(MoonGetterError.NO_INTERNET)
+    }
+    catch (_ : IllegalArgumentException) {
+        Result.Error(MoonGetterError.UNKNOWN)
+    }
+}
+
+@OptIn(ExperimentalFeature::class)
+suspend inline fun <reified T> Factory.Builder.onGetResults(urls : List<String>) : Result<T,MoonGetterError> {
+    return try {
+        val  result = get(urls).ifEmpty { return Result.Error(MoonGetterError.UNKNOWN) }
+        Result.Success(
+            result as T
+        )
+    }
+    catch (e : InvalidServerException) {
+        return when(e.error) {
+            Error.INVALID_URL_PARAMETER -> {
+                Result.Error(MoonGetterError.UNKNOWN)
+            }
+            Error.INVALID_PARAMETERS -> {
+                Result.Error(MoonGetterError.INVALID_PARAMETERS_IN_BUILDER)
+            }
+            Error.NO_PARAMETERS_TO_WORK -> {
+                Result.Error(MoonGetterError.NO_PARAMETERS_TO_WORK)
+            }
+            Error.EMPTY_OR_NULL_RESPONSE -> {
+                Result.Error(MoonGetterError.SERVERS_RESPONSE_WENT_WRONG)
+            }
+            Error.EXPECTED_RESPONSE_NOT_FOUND -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            Error.EXPECTED_PACKED_RESPONSE_NOT_FOUND -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            Error.UNSUCCESSFUL_RESPONSE -> {
+                return when (e.code ?: return Result.Error(MoonGetterError.UNKNOWN)) {
+                    401 -> Result.Error(MoonGetterError.UNAUTHORIZED)
+                    408 -> Result.Error(MoonGetterError.REQUEST_TIMEOUT)
+                    409 -> Result.Error(MoonGetterError.CONFLICT)
+                    413 -> Result.Error(MoonGetterError.PAYLOAD_TOO_LARGE)
+                    429 -> Result.Error(MoonGetterError.TOO_MANY_REQUESTS)
+                    in 500..599 -> Result.Error(MoonGetterError.SERVER_ERROR)
+                    else -> Result.Error(MoonGetterError.UNKNOWN)
+                }
+            }
+            Error.RESOURCE_TAKEN_DOWN -> {
+                Result.Error(MoonGetterError.RESOURCE_TAKEN_DOWN)
+            }
+            else -> {
+                Result.Error(MoonGetterError.UNKNOWN)
+            }
+        }
+    }
+    catch (_ : IOException) {
+        Result.Error(MoonGetterError.NO_INTERNET)
+    }
+    catch (_ : IllegalArgumentException) {
+        Result.Error(MoonGetterError.UNKNOWN)
+    }
+}
