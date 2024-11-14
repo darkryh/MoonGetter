eaa2d2550081864deab9db2831387733dffd46e5
darkryh <alex_torres-xc@hotmail.com>
2024-11-14 11:43:32 -0500

    intent class

--- app/src/main/java/com/ead/project/moongetter/presentation/main/intent/MainIntent.kt
+++ app/src/main/java/com/ead/project/moongetter/presentation/main/intent/MainIntent.kt
@@ -1,14 +1,4 @@
 package com.ead.project.moongetter.presentation.main.intent
 
-import android.content.Context
-import androidx.compose.ui.text.input.TextFieldValue
-import com.ead.lib.moongetter.models.Request
 
-
-sealed  class MainIntent {
-    data class EnteredTargetSearch(val value: TextFieldValue): MainIntent()
-    class OnGetResult(val context: Context,val url : String?=null) : MainIntent()
-    class OnGetUntilFindNewResult(val context: Context,val urls: List<String>) : MainIntent()
-    class OnGetResults(val context: Context,val urls : List<String> = emptyList()) : MainIntent()
-    class OnSelectedUrl(val request: Request) : MainIntent()
-}
+sealed interface MainIntent
