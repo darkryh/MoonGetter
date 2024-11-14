09ea1eea969e9972e3b2b458f1b31c2155515004
darkryh <alex_torres-xc@hotmail.com>
2024-11-14 11:38:55 -0500

    added InvalidServerException following properties error, to known what error its happening and code in case is unsuccessful error to handle the code response

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/models/exceptions/InvalidServerException.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/models/exceptions/InvalidServerException.kt
@@ -1,3 +1,9 @@
 package com.ead.lib.moongetter.models.exceptions
 
-class InvalidServerException (message: String) : Exception(message)
+import com.ead.lib.moongetter.models.Error
+
+class InvalidServerException (
+    message: String,
+    val error: Error,
+    val code: Int? = null,
+) : Exception(message)
