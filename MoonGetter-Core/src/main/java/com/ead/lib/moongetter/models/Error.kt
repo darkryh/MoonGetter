9b60437af2303821230d95b4ae35f521adf5b4e1
darkryh <alex_torres-xc@hotmail.com>
2024-11-22 18:14:32 -0500

    renamed some errors enums

--- MoonGetter-Core/src/main/java/com/ead/lib/moongetter/models/Error.kt
+++ MoonGetter-Core/src/main/java/com/ead/lib/moongetter/models/Error.kt
@@ -1,27 +1,37 @@
 package com.ead.lib.moongetter.models
 
 enum class Error {
+
     /**
      * The parameters implemented server working process not valid or empty
      */
-    INVALID_URL_PARAMETER,
+
+    INVALID_PROCESS_IN_EXPECTED_URL_ENTRY,
+
     /**
      * The parameters implemented in Builder are not valid or empty
      */
-    INVALID_PARAMETERS,
+
+    INVALID_BUILDER_PARAMETERS,
+
     /**
      * Parameters implemented in get() functions are not valid or empty
      */
+
     NO_PARAMETERS_TO_WORK,
+
     /**
      * The response is empty or null
      */
+
     EMPTY_OR_NULL_RESPONSE,
+
     /**
      * The response is not as expected for finding process as regex or scrapping
      * It is possible that the structure of the target site changed or the request
      * was taken down
      */
+
     EXPECTED_RESPONSE_NOT_FOUND,
 
     /**
@@ -29,13 +39,18 @@ enum class Error {
      */
 
     EXPECTED_PACKED_RESPONSE_NOT_FOUND,
+
     /**
      * The response is not successful use code : Int? property to handle exception
      */
+
     UNSUCCESSFUL_RESPONSE,
 
     /**
      * The resource was definitely taken down
+     * another case of resource was taken down
+     * check EXPECTED_RESPONSE_NOT_FOUND or EXPECTED_PACKED_RESPONSE_NOT_FOUND
      */
+
     RESOURCE_TAKEN_DOWN,
 }
