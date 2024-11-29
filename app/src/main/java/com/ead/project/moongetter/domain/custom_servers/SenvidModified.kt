6630c99d040df8ca09fc3f60d2aa7be2d644413b
darkryh <alex_torres-xc@hotmail.com>
2024-11-28 21:09:08 -0500

    updated for documented process

--- app/src/main/java/com/ead/project/moongetter/domain/custom_servers/SenvidModified.kt
+++ app/src/main/java/com/ead/project/moongetter/domain/custom_servers/SenvidModified.kt
@@ -1,11 +1,11 @@
 package com.ead.project.moongetter.domain.custom_servers
 
-import android.content.Context
+import com.ead.lib.moongetter.core.Resources
 import com.ead.lib.moongetter.core.system.extensions.await
 import com.ead.lib.moongetter.models.Configuration
-import com.ead.lib.moongetter.models.error.Error
 import com.ead.lib.moongetter.models.Server
 import com.ead.lib.moongetter.models.Video
+import com.ead.lib.moongetter.models.error.Error
 import com.ead.lib.moongetter.models.exceptions.InvalidServerException
 import com.ead.lib.moongetter.utils.PatternManager
 import okhttp3.OkHttpClient
@@ -17,19 +17,17 @@ class SenvidModified(
     configData: Configuration.Data
 ) : Server(url,client,headers,configData) {
 
-    /*private val urlRegex =  """https://custom\.domain\.com/aqua/sv\?url=([^&]+)""".toRegex()
+    private val urlRegex =  """https://custom\.domain\.com/aqua/sv\?url=([^&]+)""".toRegex()
 
     override var url: String = urlRegex.find(url)?.groupValues?.get(1) ?:
-    throw InvalidServerException(context.getString(R.string.url_provided_is_not_expected), Error.INVALID_PROCESS_IN_EXPECTED_URL_ENTRY)
-*/
+    throw InvalidServerException(Resources.invalidProcessInExpectedUrlEntry(name), Error.INVALID_PROCESS_IN_EXPECTED_URL_ENTRY)
+
     override suspend fun onExtract(): List<Video> {
-        /*val response = client
+        val response = client
             .newCall(GET())
             .await()
 
-        if (!response.isSuccessful) throw InvalidServerException(context.getString(
-            R.string.server_domain_is_down,
-            name), Error.UNSUCCESSFUL_RESPONSE, response.code)
+        if (!response.isSuccessful) throw InvalidServerException(Resources.unsuccessfulResponse(name), Error.UNSUCCESSFUL_RESPONSE, response.code)
 
         return listOf(
             Video(
@@ -37,11 +35,8 @@ class SenvidModified(
                 url = PatternManager.singleMatch(
                     string =  response.body?.string().toString(),
                     regex =  "<source src=\"(.*?)\""
-                ) ?: throw InvalidServerException(context.getString(
-                    R.string.server_requested_resource_was_taken_down,
-                    name), Error.EXPECTED_RESPONSE_NOT_FOUND)
+                ) ?: throw InvalidServerException(Resources.expectedResponseNotFound(name), Error.EXPECTED_RESPONSE_NOT_FOUND)
             )
-        )*/
-        return emptyList()
+        )
     }
 }
