33497cefea30cbc24f75964698a8c729cf1d55e7
darkryh <alex_torres-xc@hotmail.com>
2024-11-30 17:23:48 -0500

    hotfix variable structure case

--- MoonGetter-Lulustream/src/main/java/com/ead/lib/moongetter/lulustream/Lulustream.kt
+++ MoonGetter-Lulustream/src/main/java/com/ead/lib/moongetter/lulustream/Lulustream.kt
@@ -33,13 +33,20 @@ class Lulustream(
 
         if (!response.isSuccessful) throw InvalidServerException(Resources.unsuccessfulResponse(name), Error.UNSUCCESSFUL_RESPONSE, response.code)
 
+        val responseBody = response.body?.string() ?: throw InvalidServerException(Resources.emptyOrNullResponse(name), Error.EMPTY_OR_NULL_RESPONSE)
+
+        val isUnpackedResource = JsUnpacker.detect(responseBody)
+
         return listOf(
             Video(
                 quality = DEFAULT,
                 url = PatternManager.singleMatch(
-                    string = JsUnpacker.unpackAndCombine(
-                        response.body?.string() ?: throw throw InvalidServerException(Resources.emptyOrNullResponse(name), Error.EMPTY_OR_NULL_RESPONSE)
-                    ) ?: throw InvalidServerException(Resources.expectedPackedResponseNotFound(name), Error.EXPECTED_PACKED_RESPONSE_NOT_FOUND),
+                    string = if (isUnpackedResource) {
+                        JsUnpacker.unpackAndCombine(
+                            responseBody
+                        ) ?: throw InvalidServerException(Resources.expectedPackedResponseNotFound(name), Error.EXPECTED_PACKED_RESPONSE_NOT_FOUND)
+                    }
+                    else responseBody ,
                     regex = """file\s*:\s*"(https?://[^"]+\.m3u8\?[^"]*)""".trimIndent()
                 ) ?: throw InvalidServerException(Resources.expectedResponseNotFound(name), Error.EXPECTED_RESPONSE_NOT_FOUND),
                 method = "GET",
