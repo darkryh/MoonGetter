88f9f3db6077bdfaa5e6a7f75cc4d8d36ff102c1
xavier <alex_torres-xc@hotmail.com>
2025-07-07 15:05:46 -0500

    Client config

--- MoonGetter-Client/src/main/java/com/ead/lib/moongetter/client/request/Request.kt
+++ MoonGetter-Client/src/main/java/com/ead/lib/moongetter/client/request/Request.kt
@@ -1,11 +1,13 @@
 package com.ead.lib.moongetter.client.request
 
+import kotlinx.serialization.KSerializer
 
-data class Request(
+data class Request<T>(
     val method: HttpMethod,
     val url: String,
     val headers: Map<String, String> = emptyMap(),
     val queryParams: Map<String, String> = emptyMap(),
-    val body: Any? = null,
+    val body: T? = null,
+    val serializer: KSerializer<T>? = null,
     val asFormUrlEncoded: Boolean = false
 )
--- MoonGetter-Client/src/main/java/com/ead/lib/moongetter/client/response/Response.kt
+++ MoonGetter-Client/src/main/java/com/ead/lib/moongetter/client/response/Response.kt
@@ -1,11 +1,14 @@
 package com.ead.lib.moongetter.client.response
 
 import com.ead.lib.moongetter.client.response.body.ResponseBody
+import com.ead.lib.moongetter.client.response.url.Url
 
 
 interface Response {
     val statusCode: Int
     val headers: Map<String, String>
     val body: ResponseBody
+    val url : Url
+
     val isSuccess: Boolean get() = statusCode in 200..299
 }
